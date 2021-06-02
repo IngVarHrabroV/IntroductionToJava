d75d8922ee17d3fec8c1137d2465499dd05d026a
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-06-02 10:10:48 +0300

    add liner.task03

---
+++ src/com/hrabrov/introduction_to_java/section_1/linear/Task03.java
@@ -0,0 +1,19 @@
+package com.hrabrov.introduction_to_java.section_1.linear;
+
+public class Task03 {
+    /**
+     * Find expression value = (sin x + cos y) / (cos x - sin y) * tg x * y;
+     *
+     * @param x input variable;
+     * @param y input variable;
+     * @return expression value;
+     */
+
+    public static double calculateTrigonometryExpression(double x, double y) {
+        double numerator, denominator;
+
+        numerator = Math.sin(x) + Math.cos(y);
+        denominator = Math.cos(x) - Math.sin(y);
+        return numerator / denominator * Math.tan(x * y);
+    }
+}
