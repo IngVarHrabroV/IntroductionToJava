02b443d2162222a604c4322b537e056f8dc0e69f
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-06-04 10:43:13 +0300

    add loop.task01, create LoopRunner

---
+++ src/com/hrabrov/introduction_to_java/section_1/loops/LoopRunner.java
@@ -0,0 +1,8 @@
+package com.hrabrov.introduction_to_java.section_1.loops;
+
+public class LoopRunner {
+    public static void run() {
+        long resultOfTask01 = Task01.sumNumbersTo(4);
+        System.out.println("Result task 01: " + resultOfTask01);
+    }
+}
---
+++ src/com/hrabrov/introduction_to_java/section_1/loops/Task01.java
@@ -0,0 +1,21 @@
+package com.hrabrov.introduction_to_java.section_1.loops;
+
+public class Task01 {
+    /**
+     * Given x. Find the sum of all numbers from 1 to x
+     *
+     * @param x input positive integer value
+     * @return sum of all numbers from 1 to x
+     */
+
+    public static long sumNumbersTo(long x) {
+        long sum = 0;
+
+        for(int i = 1; i <= x; i++) {
+            sum += i;
+        }
+        return sum;
+
+        //return (long) (Math.pow(x, 2) + x) / 2;
+    }
+}
