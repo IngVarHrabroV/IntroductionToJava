b4c47235ab41fd6b39237d86cd885b6740433400
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-06-12 18:14:05 +0300

    add loop.task07

--- src/com/hrabrov/introduction_to_java/section_1/loops/LoopRunner.java
+++ src/com/hrabrov/introduction_to_java/section_1/loops/LoopRunner.java
@@ -1,6 +1,7 @@
 package com.hrabrov.introduction_to_java.section_1.loops;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class LoopRunner {
     public static void run() {
@@ -22,5 +23,23 @@ public class LoopRunner {
         StringBuilder resultOfTask06 = Task06.numberEquivalentSymbol();
         System.out.println("Result task 06: " + resultOfTask06);
 
+        System.out.println("Start task 07:");
+        StringBuilder resultOfTask07 = Task07.findDividerForAllNumbers(enterLongFromConsole("m"), enterLongFromConsole("n"));
+        System.out.println("Result task 07: " + resultOfTask07);
+
+    }
+
+    public static long enterLongFromConsole(String x) {
+        Scanner sc = new Scanner(System.in);
+        long a;
+
+        System.out.print("Enter " + x + " >> ");
+        while (!sc.hasNextLong()){
+            System.out.println("You cannot enter " + sc.next());
+            System.out.print("Enter " + x + " >> ");
+        }
+
+        a = sc.nextLong();
+        return a;
     }
 }
---
+++ src/com/hrabrov/introduction_to_java/section_1/loops/Task07.java
@@ -0,0 +1,45 @@
+package com.hrabrov.introduction_to_java.section_1.loops;
+
+public class Task07 {
+    /**
+     * Given two natural number: m and n. Find all divider (without 0 and 1) for each other natural number between m and n.
+     * m and n enter with keyboard.
+     *
+     * @param m input natural value
+     * @param n input natural value
+     * @return all divider for natural number between m and n.
+     */
+
+    public static StringBuilder findDividerForAllNumbers(long m, long n) {
+        StringBuilder tableWithNumbersAndDivider = new StringBuilder("\n");
+
+        if (m > n) {
+            throw new IllegalArgumentException("m cannot be large then n");
+        } else if (m == n) {
+            throw new IllegalArgumentException("m cannot be equals n");
+        } else if (m < 0) {
+            throw new IllegalArgumentException("m cannot be less then 0, try again");
+        }
+
+        for (long i = m; i <= n; i++) {
+            tableWithNumbersAndDivider.append(findDivider(i));
+            if (i != n) {
+                tableWithNumbersAndDivider.append("\n");
+            }
+        }
+
+        return tableWithNumbersAndDivider;
+    }
+
+
+    public static String findDivider(long a) {
+        String lineHeader = a + " has next divider: ";
+        String dividerForNumber = lineHeader;
+
+        for (long i = 2; i < a; i++) {
+            dividerForNumber += a % i == 0 ? i + " " : "";
+        }
+
+        return dividerForNumber.equals(lineHeader) ? a + " hasn't divider" : dividerForNumber;
+    }
+}
