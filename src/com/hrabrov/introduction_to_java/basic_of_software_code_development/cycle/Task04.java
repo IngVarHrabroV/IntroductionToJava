328c061c30ee421097985118e8b3ca3bd248a292
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-06-17 12:14:26 +0300

    fix: cycle.task04 use without exception. Alert message about overflow

---
+++ src/com/hrabrov/introduction_to_java/basic_of_software_code_development/cycle/Task04.java
@@ -0,0 +1,36 @@
+package com.hrabrov.introduction_to_java.basic_of_software_code_development.cycle;
+
+public class Task04 {
+    /**
+     * Find multiply of square two hundred numbers starting x (inclusive x) with increment h
+     *
+     * @param x input start value
+     * @param h input increment value
+     * @return multiply of square two hundred number
+     */
+
+    public static String multiplySquareTwoHundredNumbers(long x, int h) {
+        long resultMultiplyOfSquare;
+        long nextNumberOfSquare;
+
+        resultMultiplyOfSquare = 1;
+
+        int stepToNextNumber;
+        for (int i = 0; i < 200; i++) {
+            if (h > 10737418) { 
+                return "Increment is bigger, enter less increment";
+            }
+
+            stepToNextNumber = i * h;
+            nextNumberOfSquare = (long) Math.pow(x + stepToNextNumber, 2);
+
+            if (resultMultiplyOfSquare > resultMultiplyOfSquare * nextNumberOfSquare) {
+               return "Variable is overflow, enter any less start value or increment";
+            }
+            
+            resultMultiplyOfSquare = resultMultiplyOfSquare * nextNumberOfSquare ;
+        }
+        
+        return Long.toString(resultMultiplyOfSquare);
+    }
+}
