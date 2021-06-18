ee2996d72467ddc1beb12dc2da950b98a53bcdb5
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-06-18 09:50:07 +0300

    refactor: main and CycleRunner

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
-import com.hrabrov.introduction_to_java.basic_of_software_code_development.cicle.CycleRunner;
+import com.hrabrov.introduction_to_java.basic_of_software_code_development.branching.BranchingRunner;
+import com.hrabrov.introduction_to_java.basic_of_software_code_development.cycle.CycleRunner;
+import com.hrabrov.introduction_to_java.basic_of_software_code_development.linear_program.LinerRunner;
 
 public class Main {
     public static void main(String[] args) {
-       /* System.out.println("Start 1th block: branching");
+        System.out.println("Start 1th block: branching");
         LinerRunner.run();
         System.out.println("End 1th block: branching \n");
 
         System.out.println("Start 2th block: linear program");
         BranchingRunner.run();
         System.out.println("End 2th block: linear program \n");
-*/
+
         System.out.println("Start 3th block: cycle");
         CycleRunner.run();
-        System.out.println("End 3th block: loops");
+        System.out.println("End 3th block: cycle");
     }
 }
--- src/com/hrabrov/introduction_to_java/basic_of_software_code_development/cycle/CycleRunner.java
+++ src/com/hrabrov/introduction_to_java/basic_of_software_code_development/cycle/CycleRunner.java
@@ -1,4 +1,4 @@
-package com.hrabrov.introduction_to_java.basic_of_software_code_development.cicle;
+package com.hrabrov.introduction_to_java.basic_of_software_code_development.cycle;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -6,19 +6,19 @@ import java.util.Scanner;
 
 public class CycleRunner {
     public static void run() {
-       /*String resultOfTask01 = Task01.sumNumbersTo(4);
+        String resultOfTask01 = Task01.sumNumbersTo(4);
         System.out.println("Result task 01: " + resultOfTask01);
 
         String resultOfTask02 = Task02.foundFunctionValue(-1, 3,0.5);
         System.out.println("Result task 02: " + resultOfTask02);
 
-        long resultOfTask03 = Task03.sumSquareHundredNumbers(368L, 1);
-        System.out.println("Result task 03: " + resultOfTask03);*/
+        String resultOfTask03 = Task03.sumSquareHundredNumbers(368869976L, 1);
+        System.out.println("Result task 03: " + resultOfTask03);
 
         String resultOfTask04 = Task04.multiplySquareTwoHundredNumbers(1, 1);
         System.out.println("Result task 04: " + resultOfTask04);
 
-        /*double resultOfTask05 = Task05.sumMembersOfTheNumberRow(2, 0, 5);
+        double resultOfTask05 = Task05.sumMembersOfTheNumberRow(2, 0, 5);
         System.out.println("Result task 05: " + resultOfTask05);
 
         StringBuilder resultOfTask06 = Task06.numberEquivalentSymbol();
@@ -28,8 +28,8 @@ public class CycleRunner {
         StringBuilder resultOfTask07 = Task07.findDividerForAllNumbers(enterLongFromConsole("m"), enterLongFromConsole("n"));
         System.out.println("Result task 07: " + resultOfTask07);
 
-        ArrayList<Integer> resultOfTask08 = Task08.identicalNumeralsForTwoNumbers(1.111111112, 1234417.6313424645654364563456246345413451);
-        System.out.println("Result task 08: " + Arrays.toString(resultOfTask08.toArray()));*/
+        ArrayList<Integer> resultOfTask08 = Task08.identicalNumeralsForTwoNumbers(1.1111111162, 34516789.5443);
+        System.out.println("Result task 08: " + Arrays.toString(resultOfTask08.toArray()));
     }
 
     public static long enterLongFromConsole(String x) {
