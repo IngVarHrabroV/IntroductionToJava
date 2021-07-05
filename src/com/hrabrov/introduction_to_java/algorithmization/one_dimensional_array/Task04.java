67c91cd7a27804d7bed140a3d2ec5d0b08bb1285
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-07-05 14:02:37 +0300

    refactor: one_dimensional_array.task04

--- src/com/hrabrov/introduction_to_java/algorithmization/one_dimensional_array/Task04.java
+++ src/com/hrabrov/introduction_to_java/algorithmization/one_dimensional_array/Task04.java
@@ -9,39 +9,24 @@ public class Task04 {
      */
 
     public static double[] swapElements(double[] initialArray) {
-        double minElement = initialArray[0];
-        double maxElement = initialArray[0];
-        int indexMinElement = 0;
-        int indexMaxElement = 0;
+        int maxIndex = 0;
+        int minIndex = 0;
 
-        double[] finishArray = new double[initialArray.length];
+        for (int i = 1; i < initialArray.length; i++) {
+            if (initialArray[i] < initialArray[minIndex]) {
+                minIndex = i;
+            }
 
-        for (int i = 0; i < initialArray.length; i++) {
-            if (initialArray[i] < minElement) {
-                minElement = initialArray[i];
-                indexMinElement = i;
+            if (initialArray[i] > initialArray[maxIndex]) {
+                maxIndex = i;
             }
         }
 
-        for (int i = 0; i < initialArray.length; i++) {
-            if (initialArray[i] > maxElement) {
-                maxElement = initialArray[i];
-                indexMaxElement = i;
-            }
-        }
+        double temp;
+        temp = initialArray[minIndex];
+        initialArray[minIndex] = initialArray[maxIndex];
+        initialArray[maxIndex] = temp;
 
-        for (int i = 0; i < initialArray.length; i++) {
-            finishArray[i] = initialArray[i];
-
-            if (i == indexMinElement) {
-                finishArray[i] = maxElement;
-            }
-
-            if (i == indexMaxElement) {
-                finishArray[i] = minElement;
-            }
-        }
-
-        return finishArray;
+        return initialArray;
     }
 }
