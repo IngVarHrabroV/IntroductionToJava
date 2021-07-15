6ae4a707731ea01a06741b388de166fcaccccbe7
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-07-15 15:41:19 +0300

    refactor: matrixToString methods

--- src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/MultidimensionalArray.java
+++ src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/MultidimensionalArray.java
@@ -2,20 +2,26 @@ package com.hrabrov.introduction_to_java.algorithmization.multidimensional_array
 
 public class MultidimensionalArray {
     public static void run() {
-        int[][] matrixForTask01 = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrixForTask01 = new int[][]{
+                {25, 4, 3, 5, 12, 2, 2, 2, 2, 2, 2},
+                {8, 7, 6, 6, 7},
+                {23, 6, 5, 11, 2, 1, 1, 1}
+        };
         int[][] resultOfTask01 = Task01.foundOddColumnsWithGreaterFirstItem(matrixForTask01);
-        System.out.println(matrixToString(resultOfTask01));
+        System.out.println("Result of task 01:");
+        System.out.println("Original matrix:\n" + matrixToString(matrixForTask01));
+        System.out.println("Transformed matrix:\n" + matrixToString(resultOfTask01));
     }
 
-    private static String matrixToString(int[][] matrix) {
+    public static String matrixToString(int[][] matrix) {
         String matrixToString = "";
 
-        for (int i = 0; i < matrix.length; i++){
+        for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 matrixToString += matrix[i][j];
                 matrixToString += (j == matrix[i].length - 1) ? "" : "\t";
             }
-            matrixToString += "\n";
+            matrixToString += (i == matrix.length - 1) ? "" : "\n";
         }
         return matrixToString;
     }
