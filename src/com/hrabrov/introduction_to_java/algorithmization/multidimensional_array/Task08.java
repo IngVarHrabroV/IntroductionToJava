411b779255b88506064507657cc8c6c600a27618
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-07-17 10:52:35 +0300

    add: multidimensional_array.Task08

---
+++ src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/Task08.java
@@ -0,0 +1,30 @@
+package com.hrabrov.introduction_to_java.algorithmization.multidimensional_array;
+
+public class Task08 {
+    /**
+     * В числовой матрице поменять местами два любых столбца,
+     * т. е. все элементы одного столбца поставить на соответствующие им позиции другого,
+     * а элементы второго столбца переместить в первый.
+     * Номера столбцов вводит пользователь с клавиатуры.
+     *
+     * Given numeral matrix. Replace the position of any two columns.
+     * User enter is column numbers from keyboard
+     *
+     * @param originalMatrix input numeral matrix
+     * @param firstColumn input number on the first column to replace
+     * @param secondColumn input number on the second column to replace
+     * @return matrix with replaced columns
+     */
+
+    public static int[][] replacedTwoColumn(int[][] originalMatrix, int firstColumn, int secondColumn) {
+
+        int temp;
+        for (int i = 0; i < originalMatrix.length; i++) {
+            temp = originalMatrix[i][firstColumn - 1];
+            originalMatrix[i][firstColumn - 1] = originalMatrix[i][secondColumn - 1];
+            originalMatrix[i][secondColumn - 1] = temp;
+        }
+
+        return originalMatrix;
+    }
+}
