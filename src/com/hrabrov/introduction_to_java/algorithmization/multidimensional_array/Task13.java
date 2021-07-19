c3cd6add57ee9542a0bba1081f0e1f8b563f9eb9
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-07-19 15:34:00 +0300

    add: multidimensional_array.Task13

---
+++ src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/Task13.java
@@ -0,0 +1,46 @@
+package com.hrabrov.introduction_to_java.algorithmization.multidimensional_array;
+
+public class Task13 {
+    /**
+     * Отсотрировать стобцы матрицы по возрастанию и убыванию значений эементов.
+     * Sort matrix column in ascending or descending order of elements value.
+     *
+     * @param originalMatrix input multidimensional array like matrix
+     * @return sorted matrices in ascending or descending order
+     */
+
+    public static int[][] ascendingSortMatrix(int[][] originalMatrix) {
+
+        for (int j = 0; j < originalMatrix[0].length; j++) {
+            for (int i = 0; i < originalMatrix.length; i++) {
+                for (int k = i + 1; k < originalMatrix.length; k++) {
+                    if (originalMatrix[i][j] > originalMatrix[k][j]) {
+                        int temp;
+                        temp = originalMatrix[i][j];
+                        originalMatrix[i][j] = originalMatrix[k][j];
+                        originalMatrix[k][j] = temp;
+                    }
+                }
+            }
+        }
+
+        return originalMatrix;
+    }
+
+    public static int[][] descendingSortMatrix(int[][] originalMatrix) {
+        for (int j = 0; j < originalMatrix[0].length; j++) {
+            for (int i = 0; i < originalMatrix.length; i++) {
+                for (int k = i + 1; k < originalMatrix.length; k++) {
+                    if (originalMatrix[i][j] < originalMatrix[k][j]) {
+                        int temp;
+                        temp = originalMatrix[i][j];
+                        originalMatrix[i][j] = originalMatrix[k][j];
+                        originalMatrix[k][j] = temp;
+                    }
+                }
+            }
+        }
+
+        return originalMatrix;
+    }
+}
