15baa64fe6daebb3756f2a3bbca4561718be4dbe
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-07-23 11:19:50 +0300

    refactor: OneDimensionalArray remove Arrays class from code

--- src/com/hrabrov/introduction_to_java/algorithmization/one_dimensional_array/OneDimensionalArray.java
+++ src/com/hrabrov/introduction_to_java/algorithmization/one_dimensional_array/OneDimensionalArray.java
@@ -1,7 +1,5 @@
 package com.hrabrov.introduction_to_java.algorithmization.one_dimensional_array;
 
-import java.util.Arrays;
-
 public class OneDimensionalArray {
     public static void run() {
         int[] arrayForTask01 = new int[]{3, 4, 5, 6, 8};
@@ -12,7 +10,7 @@ public class OneDimensionalArray {
         Task02.ArrayAndCountReplacement resultOfTask02 = Task02.replacementNumber(arrayForTask02, 3.55);
         System.out.println(
                 "Result of task 02: "
-                + "replacement array = " + Arrays.toString(resultOfTask02.sequenceOfReal) + ", "
+                + "replacement array = " + arrayToString(resultOfTask02.sequenceOfReal) + ", "
                 + "number of replace = " + resultOfTask02.replacement);
 
         Task03.Quantity resultOfTask03 = Task03.checkArrayElements(10);
@@ -24,11 +22,11 @@ public class OneDimensionalArray {
 
         double[] arrayForTask04 = new double[]{4.25, 4.26, 4.27, 4.28};
         double[] resultOfTask04 = Task04.swapElements(arrayForTask04);
-        System.out.println("Result of task 04: " + Arrays.toString(resultOfTask04));
+        System.out.println("Result of task 04: " + arrayToString(resultOfTask04));
 
         int[] arrayForTask05 = new int[]{-5, 2, 5, 6, -3, -42};
         int[] resultOfTask05 = Task05.selectElement(arrayForTask05);
-        System.out.println("Result of task 05: " + Arrays.toString(resultOfTask05));
+        System.out.println("Result of task 05: " + arrayToString(resultOfTask05));
 
         double[] arrayForTask06 = new double[]{0.98, 0, -0.61, 0.38, 0.23, 0.51, -0.73, -0.04};
         double resultOfTask06 = Task06.sumPrimeNumber(arrayForTask06);
@@ -41,7 +39,7 @@ public class OneDimensionalArray {
 
         int[] arrayForTask08 = new int[]{-5, -5, -3, -1, 0, 9, 9, 1, 4, 5, -5};
         int[] resultOfTask08 = Task08.excludeMinValue(arrayForTask08);
-        System.out.println("Result of task 08: " + Arrays.toString(resultOfTask08));
+        System.out.println("Result of task 08: " + arrayToString(resultOfTask08));
 
         int[] arrayForTask09 = new int[]{1,1,1,1,2,2,2,2};
         int resultOfTask09 = Task09.mostCommonNumber(arrayForTask09);
@@ -49,6 +47,26 @@ public class OneDimensionalArray {
 
         int[] arrayForTask10 = new int[]{719, 303, 853, 745, 196, 895, 175, 292, 611, 893};
         int[] resultOfTask10 = Task10.zeroIntoSecondElement(arrayForTask10);
-        System.out.println("Result of task 10: " + Arrays.toString(resultOfTask10));
+        System.out.println("Result of task 10: " + arrayToString(resultOfTask10));
+    }
+
+    private static String arrayToString(int[] array) {
+        String arrayToString = "";
+
+        for (int i = 0; i < array.length; i++) {
+            arrayToString += array[i] + " ";
+        }
+
+        return arrayToString;
+    }
+
+    private static String arrayToString(double[] array) {
+        String arrayToString = "";
+
+        for (int i = 0; i < array.length; i++) {
+            arrayToString += array[i] + " ";
+        }
+
+        return arrayToString;
     }
 }
