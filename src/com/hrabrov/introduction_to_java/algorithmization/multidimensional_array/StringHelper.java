844d9d459e4737c218fbe7836bd207e2c546b188
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-07-30 09:22:29 +0300

    refactor: multidimensional_array.StringHelper rename variable which contain string with matrix variables

--- src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/StringHelper.java
+++ src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/StringHelper.java
@@ -2,43 +2,43 @@ package com.hrabrov.introduction_to_java.algorithmization.multidimensional_array
 
 public class StringHelper {
     public String matrixToString(int[][] matrix) {
-        String matrixToString = "";
+        String stringForStorageOfMatrixValues = "";
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                matrixToString += matrix[i][j] >= 0 ? " " : "";
-                matrixToString += matrix[i][j];
-                matrixToString += matrix[i][j] < 100 ? "\t" : "";
+                stringForStorageOfMatrixValues += matrix[i][j] >= 0 ? " " : "";
+                stringForStorageOfMatrixValues += matrix[i][j];
+                stringForStorageOfMatrixValues += matrix[i][j] < 100 ? "\t" : "";
             }
-            matrixToString += (i == matrix.length - 1) ? "" : "\n";
+            stringForStorageOfMatrixValues += (i == matrix.length - 1) ? "" : "\n";
         }
 
-        return matrixToString;
+        return stringForStorageOfMatrixValues;
     }
 
     public String matrixToString(double[][] matrix) {
-        String matrixToString = "";
+        String stringForStorageOfMatrixValues = "";
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 String temp = String.format("%.3f", matrix[i][j]);
-                matrixToString += temp;
-                matrixToString += matrix[i][j] > 0 ? " " : "";
-                matrixToString += (j == matrix[i].length - 1) ? "" : "\t";
+                stringForStorageOfMatrixValues += temp;
+                stringForStorageOfMatrixValues += matrix[i][j] > 0 ? " " : "";
+                stringForStorageOfMatrixValues += (j == matrix[i].length - 1) ? "" : "\t";
             }
-            matrixToString += (i == matrix.length - 1) ? "" : "\n";
+            stringForStorageOfMatrixValues += (i == matrix.length - 1) ? "" : "\n";
         }
 
-        return matrixToString;
+        return stringForStorageOfMatrixValues;
     }
 
     public String arrayToString(int[] array) {
-        String arrayToString = "";
+        String stringForStorageOfArrayValues = "";
 
         for (int i = 0; i < array.length; i++) {
-            arrayToString += array[i] + " ";
+            stringForStorageOfArrayValues += array[i] + " ";
         }
 
-        return arrayToString;
+        return stringForStorageOfArrayValues;
     }
 }
