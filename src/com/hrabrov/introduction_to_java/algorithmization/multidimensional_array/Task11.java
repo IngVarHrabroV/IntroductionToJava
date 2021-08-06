8614221c1b54cf699ab7479661f9f0653c938096
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-08-06 23:29:02 +0300

    refactor: multidimensional_array.Task11

--- src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/Task11.java
+++ src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/Task11.java
@@ -1,65 +1,69 @@
 package com.hrabrov.introduction_to_java.algorithmization.multidimensional_array;
 
 public class Task11 {
+    final static int CONCEIVED_NUMBER = 5;
+
     /**
      * Fill the matrix 10 on 20 with random numbers from 0 to 15.
      * Display the matrix and row numbers in which the number 5 occurs three and more times.
      *
      * @return matrix and array with row number
      */
-
+    
     private static MatrixAndRowsNumbers createMatrixForTask11() {
         MatrixAndRowsNumbers matrixAndRowsNumbers = new MatrixAndRowsNumbers();
 
-        int verticalSizeOfMatrix = 20;
-        int horizontalSizeOfMatrix = 10;
+        final int unsuitableRow = -1;
+        final int verticalSizeOfMatrix = 20;
+        final int horizontalSizeOfMatrix = 10;
+        final int maxValueForFill = 15;
 
         int[][] matrix = new int[verticalSizeOfMatrix][horizontalSizeOfMatrix];
 
         for (int i = 0; i < verticalSizeOfMatrix; i++) {
             for (int j = 0; j < horizontalSizeOfMatrix; j++) {
-                matrix[i][j] = (int) (Math.random() * 16);
+                matrix[i][j] = (int) (Math.random() * (maxValueForFill + 1));
             }
         }
 
-        int[] checkRowOnQuantityFive = new int[verticalSizeOfMatrix];
+        int[] checkRowOnQuantityConceivedNumber = new int[verticalSizeOfMatrix];
         int tempIndex = 0;
         for (int i = 0; i < verticalSizeOfMatrix; i++) {
-            int quantityFiveInRow = 0;
+            int quantityConceivedNumberInRow = 0;
             for (int j = 0; j < horizontalSizeOfMatrix; j++) {
-                if (matrix[i][j] == 5) {
-                    quantityFiveInRow++;
+                if (matrix[i][j] == CONCEIVED_NUMBER) {
+                    quantityConceivedNumberInRow++;
                 }
             }
 
-            if (quantityFiveInRow >= 3) {
-                checkRowOnQuantityFive[tempIndex] = i + 1;
+            if (quantityConceivedNumberInRow >= 3) {
+                checkRowOnQuantityConceivedNumber[tempIndex] = i + 1;
             } else {
-                checkRowOnQuantityFive[tempIndex] = -1;
+                checkRowOnQuantityConceivedNumber[tempIndex] = unsuitableRow;
             }
 
             tempIndex++;
         }
 
-        int arraySize = 0;
+        int sizeOfArray = 0;
         for (int i = 0; i < verticalSizeOfMatrix; i++) {
-            if (checkRowOnQuantityFive[i] > -1) {
-                arraySize++;
+            if (checkRowOnQuantityConceivedNumber[i] > unsuitableRow) {
+                sizeOfArray++;
             }
         }
 
         int tempIndexForArrayWithRowNumbers = 0;
-        int[] arrayWithRowNumbersOccursFiveThreePlusTime = new int[arraySize];
+        int[] rowNumbersOccursConceivedNumberThreePlusTime = new int[sizeOfArray];
         for (int i = 0; i < verticalSizeOfMatrix; i++) {
-            if (checkRowOnQuantityFive[i] > -1) {
-                arrayWithRowNumbersOccursFiveThreePlusTime[tempIndexForArrayWithRowNumbers] =
-                        checkRowOnQuantityFive[i];
+            if (checkRowOnQuantityConceivedNumber[i] > unsuitableRow) {
+                rowNumbersOccursConceivedNumberThreePlusTime[tempIndexForArrayWithRowNumbers]
+                        = checkRowOnQuantityConceivedNumber[i];
                 tempIndexForArrayWithRowNumbers++;
             }
         }
 
         matrixAndRowsNumbers.createdMatrix = matrix;
-        matrixAndRowsNumbers.rowsNumbersWithFive = arrayWithRowNumbersOccursFiveThreePlusTime;
+        matrixAndRowsNumbers.rowsNumbersWithFive = rowNumbersOccursConceivedNumberThreePlusTime;
 
         return matrixAndRowsNumbers;
     }
@@ -73,10 +77,11 @@ public class Task11 {
         System.out.println("Result of task 11:");
         MatrixAndRowsNumbers resultOfTask11 = Task11.createMatrixForTask11();
         System.out.println(StringHelper.matrixToString(resultOfTask11.createdMatrix));
+
         if (resultOfTask11.rowsNumbersWithFive.length == 0) {
-            System.out.println("**!!No row which the number 5 occurs three and more times!!**");
+            System.out.println("**!!No row which the number " + CONCEIVED_NUMBER + " occurs three and more times!!**");
         } else {
-            System.out.println("Numbers row in which the number 5 occurs three and more times: "
+            System.out.println("Numbers row in which the number " + CONCEIVED_NUMBER + " occurs three and more times: "
                     + StringHelper.arrayToString(resultOfTask11.rowsNumbersWithFive));
         }
     }
