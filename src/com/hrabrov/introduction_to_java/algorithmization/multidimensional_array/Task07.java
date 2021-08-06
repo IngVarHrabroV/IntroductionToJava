fe2db7e286c1e2def8501d553d979717dbcbd453
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-08-06 16:06:44 +0300

    refactor: multidimensional_array.Task07

--- src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/Task07.java
+++ src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/Task07.java
@@ -10,6 +10,8 @@ public class Task07 {
      */
 
     private static MatrixAndHerPositiveElements createMatrixForTask07(int sizeOfMatrix) {
+        final int DEGREE = 2;
+
         MatrixAndHerPositiveElements matrixAndHerPositiveElements = new MatrixAndHerPositiveElements();
 
         double[][] matrix = new double[sizeOfMatrix][sizeOfMatrix];
@@ -18,7 +20,7 @@ public class Task07 {
         double expressionValue;
         for (int i = 0; i < sizeOfMatrix; i++) {
             for (int j = 0; j < sizeOfMatrix; j++) {
-                expressionValue = (Math.pow(i, 2) - Math.pow(j, 2)) / sizeOfMatrix;
+                expressionValue = (Math.pow(i, DEGREE) - Math.pow(j, DEGREE)) / sizeOfMatrix;
                 matrix[i][j] = Math.sin(expressionValue);
 
                 if (matrix[i][j] > 0) {
