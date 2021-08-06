fe6527026a6a1674ba2efeed9208a5349a69e450
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-08-07 00:01:54 +0300

    refactor: multidimensional_array.Task14

--- src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/Task14.java
+++ src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/Task14.java
@@ -3,7 +3,7 @@ package com.hrabrov.introduction_to_java.algorithmization.multidimensional_array
 public class Task14 {
     /**
      * Generate a random m on n matrix, containing of zeros and ones.
-     * The number of ones in each columns is equals the column number.
+     * The number of ones in each column is equals the column number.
      *
      * @param verticalSizeOfMatrix input vertical size matrix (quantity column)
      * @param horizontalSizeOfMatrix input horizontal size matrix (quantity row)
@@ -12,27 +12,29 @@ public class Task14 {
 
     private static int[][] generateMatrixForTask14(int verticalSizeOfMatrix, int horizontalSizeOfMatrix) {
         if (checkValidateArgument(verticalSizeOfMatrix, horizontalSizeOfMatrix)) {
+            final int firstNumberForFilling = 1;
+            final int secondNumberForFilling = 0;
+
             int[][] matrix = new int[verticalSizeOfMatrix][horizontalSizeOfMatrix];
 
             for (int j = 0; j < matrix[0].length; j++) {
-                int countOne = 0;
+                int countFirstNumberForFilling = 0;
                 for (int i = 0; i < matrix.length; i++) {
-                    if (countOne != j + 1) {
-                        int howManyOneNeed;
-                        howManyOneNeed = j + 1 - countOne;
+                    if (countFirstNumberForFilling != j + 1) {
+                        int howManyFirstNumbersNeed = j + 1 - countFirstNumberForFilling;
 
-                        if (howManyOneNeed == matrix.length - i) {
-                            matrix[i][j] = 1;
-                            countOne++;
+                        if (howManyFirstNumbersNeed == matrix.length - i) {
+                            matrix[i][j] = firstNumberForFilling;
+                            countFirstNumberForFilling++;
                         } else {
-                            matrix[i][j] = (int) (Math.random() * 2);
-                            if (matrix[i][j] == 1) {
-                                countOne++;
+                            matrix[i][j] = (int) (Math.random() * (firstNumberForFilling + 1));
+                            if (matrix[i][j] == firstNumberForFilling) {
+                                countFirstNumberForFilling++;
                             }
                         }
 
                     } else {
-                        matrix[i][j] = 0;
+                        matrix[i][j] = secondNumberForFilling;
                     }
                 }
             }
@@ -40,7 +42,7 @@ public class Task14 {
             return matrix;
         }
 
-        return new int[0][0];
+        return null;
     }
 
     private static boolean checkValidateArgument(int m, int n) {
@@ -62,6 +64,11 @@ public class Task14 {
     public static void run(int verticalSizeOfMatrix, int horizontalSizeOfMatrix) {
         System.out.println("Result of task 14:");
         int[][] resultOfTask14 = Task14.generateMatrixForTask14(verticalSizeOfMatrix, horizontalSizeOfMatrix);
+
+        if (resultOfTask14 == null) {
+            return;
+        }
+
         System.out.println(StringHelper.matrixToString(resultOfTask14));
     }
 }
