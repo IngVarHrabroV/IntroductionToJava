9ac6654f1ec36871b95e13583cdd37e6d5e4b272
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-08-06 16:30:42 +0300

    refactor: multidimensional_array.Task08

--- src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/Task08.java
+++ src/com/hrabrov/introduction_to_java/algorithmization/multidimensional_array/Task08.java
@@ -3,6 +3,7 @@ package com.hrabrov.introduction_to_java.algorithmization.multidimensional_array
 import java.util.Scanner;
 
 public class Task08 {
+
     /**
      * Given numeral matrix. Replace the position of any two columns.
      * User enter is column numbers from keyboard
@@ -12,19 +13,19 @@ public class Task08 {
      */
 
     private static int[][] replaceTwoColumn(int[][] originalMatrix) {
-        int matrixVerticalSize = originalMatrix.length;
-        int matrixHorizontalSize = originalMatrix[0].length;
+        final int VERTICAL_SIZE_OF_MATRIX = originalMatrix.length;
+        final int HORIZONTAL_SIZE_OF_MATRIX = originalMatrix[0].length;
 
         System.out.print("Enter number of first column for replace: ");
-        int numberFirstColumn = inputIntNumber();
+        final int NUMBER_FIRST_COLUMN = inputIntNumber();
         System.out.print("Enter number of second column for replace: ");
-        int numberSecondColumn = inputIntNumber();
+        final int NUMBER_SECOND_COLUMN = inputIntNumber();
 
-        if (checkValidationArgument(matrixHorizontalSize, numberFirstColumn, numberSecondColumn)) {
-            for (int i = 0; i < matrixVerticalSize; i++) {
-                int temp = originalMatrix[i][numberFirstColumn - 1];
-                originalMatrix[i][numberFirstColumn - 1] = originalMatrix[i][numberSecondColumn - 1];
-                originalMatrix[i][numberSecondColumn - 1] = temp;
+        if (checkValidationArgument(HORIZONTAL_SIZE_OF_MATRIX, NUMBER_FIRST_COLUMN, NUMBER_SECOND_COLUMN)) {
+            for (int i = 0; i < VERTICAL_SIZE_OF_MATRIX; i++) {
+                int temp = originalMatrix[i][NUMBER_FIRST_COLUMN - 1];
+                originalMatrix[i][NUMBER_FIRST_COLUMN - 1] = originalMatrix[i][NUMBER_SECOND_COLUMN - 1];
+                originalMatrix[i][NUMBER_SECOND_COLUMN - 1] = temp;
             }
 
             return originalMatrix;
@@ -57,11 +58,12 @@ public class Task08 {
         System.out.println(StringHelper.matrixToString(originalMatrix));
 
         System.out.println("Starting replace column");
-
         int[][] resultOfTask08 = Task08.replaceTwoColumn(originalMatrix);
+
         if (resultOfTask08 == null) {
             return;
         }
+
         System.out.println("Matrix with replaced columns: ");
         System.out.println(StringHelper.matrixToString(resultOfTask08));
     }
