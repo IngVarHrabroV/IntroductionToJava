488a4b6ffe3c9f7de0c232edf1b6a8cb120dfe83
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-08-09 19:36:16 +0300

    add: decomposition.Task04

---
+++ src/com/hrabrov/introduction_to_java/algorithmization/decomposition/task04/Dot.java
@@ -0,0 +1,11 @@
+package com.hrabrov.introduction_to_java.algorithmization.decomposition.task04;
+
+public class Dot {
+    final int x;
+    final int y;
+
+    Dot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/com/hrabrov/introduction_to_java/algorithmization/decomposition/task04/Task04.java
@@ -0,0 +1,68 @@
+package com.hrabrov.introduction_to_java.algorithmization.decomposition.task04;
+
+public class Task04 {
+    /**
+     * Given n dots on plane. Find, which dots have max distance
+     *
+     * @param coordinates input X and Y coordinate like [x1, y1, x2, y2, ..., xn, yn]
+     * @return coordinates of two dots
+     */
+
+    private static Dot[] findMaxDistance(int[] coordinates) {
+        final double startMaxDistance = 0.0;
+        Dot[] dots = createDots(coordinates);
+        Dot[] dotsWithMaxDistance = new Dot[2];
+
+        double maxDistance = startMaxDistance;
+        for (int i = 0; i < dots.length - 1; i++) {
+            for (int j = i + 1; j < dots.length; j++) {
+                double distance = calculateDistance(dots[i], dots[j]);
+                if (distance > maxDistance) {
+                    maxDistance = distance;
+                    dotsWithMaxDistance[0] = dots[i];
+                    dotsWithMaxDistance[1] = dots[j];
+                }
+            }
+        }
+        return dotsWithMaxDistance;
+    }
+
+    private static double calculateDistance(Dot a, Dot b) {
+        if (a.x == b.x) {
+            return Math.abs(a.y - b.y);
+        }
+
+        if (a.y == b.y) {
+            return Math.abs(a.x - b.x);
+        }
+
+        return calculateByTheorem(a, b);
+    }
+
+    private static double calculateByTheorem(Dot a, Dot b) {
+        final double firstLegSquared = Math.pow((a.x - b.x), 2);
+        final double secondLegSquared = Math.pow((a.y - b.y), 2);
+
+        return Math.sqrt(firstLegSquared + secondLegSquared);
+    }
+
+    private static Dot[] createDots(int[] coordinates) {
+        final int sizeOfDotsArray = coordinates.length / 2;
+        Dot[] dots = new Dot[sizeOfDotsArray];
+
+        for (int i = 0; i < sizeOfDotsArray; i++) {
+            dots[i] = new Dot(coordinates[i * 2], coordinates[i * 2 + 1]);
+        }
+
+        return dots;
+    }
+
+    public static void run(int[] coordinates) {
+        System.out.println("Result of task 04:");
+        Dot[] resultOfTask04 = findMaxDistance(coordinates);
+        System.out.println("Max distance between dots with coordinate ("
+                + resultOfTask04[0].x + ";" + resultOfTask04[0].y + ") and ("
+                + resultOfTask04[1].x + ";" + resultOfTask04[1].y + ") and equals = "
+                + String.format("%.3f", calculateDistance(resultOfTask04[0], resultOfTask04[1])));
+    }
+}
