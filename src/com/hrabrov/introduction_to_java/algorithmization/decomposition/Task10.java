6c1dce8b823743962085acc08796a58aa9f7b1f0
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-08-11 16:27:55 +0300

    add: decomposition.Task10

---
+++ src/com/hrabrov/introduction_to_java/algorithmization/decomposition/Task10.java
@@ -0,0 +1,44 @@
+package com.hrabrov.introduction_to_java.algorithmization.decomposition;
+
+public class Task10 {
+    /**
+     * Given a natural number n. Create array, elements of which are the digits of the number n.
+     *
+     * @param n input a natural number
+     * @return array are the digits of the number n
+     */
+
+    private static int[] numberToArray(int n) {
+        final int digitOfNumber = foundDigitOfNumber(n);
+        int[] array = new int[digitOfNumber];
+
+        return feelArray(array, n);
+    }
+
+    private static int[] feelArray(int[] array, int n) {
+        for (int i = array.length - 1; i >= 0; i--) {
+            array[i] = n % 10;
+            n /= 10;
+        }
+
+        return array;
+    }
+
+    private static int foundDigitOfNumber(int n) {
+        int digit = 1;
+        while (n / (int) Math.pow(10, digit) != 0) {
+            digit++;
+        }
+
+        return digit;
+    }
+
+    public static void run(int n) {
+        if (!Helper.isNatural(n)) {
+            return;
+        }
+
+        int[] resultOfTask10 = numberToArray(n);
+        System.out.println("Result of task 10: " + Helper.arrayToString(resultOfTask10));
+    }
+}
